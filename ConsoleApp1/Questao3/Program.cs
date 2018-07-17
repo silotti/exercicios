efef29846d1aa5e9b4d7535bd9f23b80dcb38622
Leonardo Luiz Silotti <silotti@gmail.com>
2018-07-17 09:43:54 -0300

    Questão 3 - OK

--- ConsoleApp1/Questao3/Program.cs
+++ ConsoleApp1/Questao3/Program.cs
@@ -12,11 +12,57 @@ namespace Questao3
         static void Main(string[] args)
         {
 
-            // cria o diretório Temp2
-            Directory.CreateDirectory("C:\\Temp2");
+            // cria o diretório temp2
+            Directory.CreateDirectory("C:\\temp2");
 
-            //Copiando um arquivo do diretório C:, para D:
-            File.Copy("C:\\Temp\\imagem.jpg", "C:\\Temp2\\imagem.jpg");
+            //Copiando um arquivo do diretório C:\temp, para C\temp2:
+
+            string sourceDir = @"c:\temp";
+            string backupDir = @"c:\temp2";
+
+            try
+            {
+                string[] picList = Directory.GetFiles(sourceDir, "*.jpg");
+                string[] txtList = Directory.GetFiles(sourceDir, "*.txt");
+
+                // Copy picture files.
+                foreach (string f in picList)
+                {
+                    // Remove path from the file name.
+                    string fName = f.Substring(sourceDir.Length + 1);
+
+                    // Use the Path.Combine method to safely append the file name to the path.
+                    // Will overwrite if the destination file already exists.
+                    File.Copy(Path.Combine(sourceDir, fName), Path.Combine(backupDir, fName), true);
+                }
+
+                // Copy text files.
+                foreach (string f in txtList)
+                {
+
+                    // Remove path from the file name.
+                    string fName = f.Substring(sourceDir.Length + 1);
+
+                    try
+                    {
+                        // Will not overwrite if the destination file already exists.
+                        File.Copy(Path.Combine(sourceDir, fName), Path.Combine(backupDir, fName));
+                    }
+
+                    // Catch exception if the file was already copied.
+                    catch (IOException copyError)
+                    {
+                        Console.WriteLine(copyError.Message);
+                    }
+                }
+
+            }
+
+            catch (DirectoryNotFoundException dirNotFound)
+            {
+                Console.WriteLine(dirNotFound.Message);
+            }
+
 
             Console.Read();
         }
